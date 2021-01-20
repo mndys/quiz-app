a1fc0ed34a4fe64678a024ab34a480214e9d8b49
Mandy Schwarze <mandy2301@me.com>
2021-01-20 22:29:09 +0100

    Setup storybook to work on "nav"

---
+++ components/nav.js
@@ -0,0 +1,11 @@
+import '../styles/index.css'
+
+import './nav.css'
+import html from './nav.html'
+
+export default {
+  title: 'Navigation',
+  component: html,
+}
+
+export const base = () => html
